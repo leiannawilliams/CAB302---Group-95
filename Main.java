50fa0344e1fee32e47b192d37636674aa9bfe044
pcamp44 <paulinecampos13@gmail.com>
2022-06-03 13:05:26 +1000

    Set cross-platform look & feel

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 import GUI.mainWindow;
 
+import javax.swing.*;
+
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
         mainWindow window = new mainWindow();
         window.createMainWindow(mainWindow.project);
     }
