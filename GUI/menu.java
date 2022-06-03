0e8913d0efa0015e20db17cc7eb078b5be2365a5
pcamp44 <paulinecampos13@gmail.com>
2022-06-04 02:37:34 +1000

    Added a 'Rename' option to file menu

--- GUI/menu.java
+++ GUI/menu.java
@@ -15,16 +15,18 @@ public abstract class menu extends JFrame{
         JMenuItem newProject = fileMenu.add("New Project");
         JMenuItem viewProjects = fileMenu.add("View Projects");
         // Action Listener to view list of maze projects (this will direct the user to the maze list window)
-        viewProjects.addActionListener(e -> {
-            mazeList maze_list = new mazeList();
-            maze_list.createMazeList();
-        });
+        viewProjects.addActionListener(e -> mazeList.createMazeList());
         JMenuItem save = fileMenu.add("Save");
         JMenuItem export = fileMenu.add("Export");
         menuBar.add(fileMenu);
 
         // EDIT MENU
         JMenu editMenu = new JMenu("Edit");
+        JMenuItem rename = editMenu.add("Rename Project");
+        // Action Listener to rename project
+        rename.addActionListener(e -> {
+            String renameProject = JOptionPane.showInputDialog(null, "Rename Project");
+        });
         JMenuItem undo = editMenu.add("Undo");
         JMenuItem delete = editMenu.add("Delete");
         menuBar.add(editMenu);
