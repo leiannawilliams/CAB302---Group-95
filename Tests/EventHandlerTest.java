4ff55475bde5e5aaa2677c77919f8cc3f16f05dc
pcamp44 <paulinecampos13@gmail.com>
2022-06-12 20:04:21 +1000

    Created package for unit tests

---
+++ Tests/EventHandlerTest.java
@@ -0,0 +1,18 @@
+package Tests;
+
+import org.junit.jupiter.api.Test;
+
+class EventHandlerTest {
+
+    @Test
+    void browseButtonClicked() {
+    }
+
+    @Test
+    void generateButtonClicked() {
+    }
+
+    @Test
+    void actionPerformed() {
+    }
+}
